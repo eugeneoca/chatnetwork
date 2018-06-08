6b1473d3bd061882dfebabf5e95d5dac83060ad8
eugeneoca <eugenebasiliscooca@gmail.com>
2018-06-09 00:46:30 +0800

    Added and worked with welcome screen

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
+// Libraries
 #include <iostream>
+#include <cmath>
+#include <unistd.h>
+
 using namespace std;
 
+// Method Initialization
+int welcome_screen();
+
 int main() {
-   cout << "Hello World\n";
-   return 0;
+    system("cls");
+    if(welcome_screen()){
+        return 0;
+    }
+    return 0;
+}
+
+int welcome_screen(){
+    cout << "ChatNetwork 1.0\n";
+    cout << "Developed by Eugene Oca\n";
+    sleep(1);
+    cout << "Loading...\n";
+    sleep(1);
+    cout << "Gathering system information...\n";
+    sleep(1);
+    if(system("ver")){
+        cout << system("ver\n");
+    }else if(_WIN64 && _WIN32){
+        cout << "64-bit WINDOWS\n";
+    }else if(_WIN32){
+        cout << "32-bit WINDOWS\n";
+    }else{
+        cout << "Please consider using WINDOWS Operating System.\n";
+        return 1;
+    }
+    return 0;
 }
