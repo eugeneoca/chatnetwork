49823b5c5213e2c691391372711686dd2b982e3c
eugeneoca <eugenebasiliscooca@gmail.com>
2018-06-08 23:45:20 +0800

    Created builder.bat for C++ Compiling

---
+++ builder.bat
@@ -0,0 +1,14 @@
+@echo off
+rem ChatNetwork Builder
+title ChatNetwork
+color 0a
+cls
+
+echo Compiling...
+g++ main.cpp -o main.exe
+echo Session Starting...
+echo ------------------------
+main.exe
+echo ------------------------
+echo Session ended.
+@echo on
