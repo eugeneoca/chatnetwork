e87603ea6859faffaa4d8d169b0cd31c7cc56ab9
eugeneoca <eugenebasiliscooca@gmail.com>
2018-06-09 15:25:47 +0800

    Added server files

---
+++ serve.bat
@@ -0,0 +1,15 @@
+@echo off
+rem ChatNetwork Server Builder
+title ChatNetwork Server
+color 0a
+cls
+
+echo Compiling...
+g++ server.cpp -o server.exe
+echo Session Starting...
+echo ------------------------
+server.exe
+del server.exe
+echo ------------------------
+echo Session ended.
+@echo on
