143770ea7bc9608cac9484a47eb724f6fb27af9a
eugeneoca <eugenebasiliscooca@gmail.com>
2018-06-09 14:35:53 +0800

    Updated the welcome screen and added some information gathering

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 // Libraries
 #include <iostream>
-#include <cmath>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
 #include <unistd.h>
 
 using namespace std;
 
 // Method Initialization
 int welcome_screen();
+string getIPAddress();
+string getSubnet();
+
+// Global Variables
+string client_ip;
+string client_subnet;
+int port = 2000;
 
 int main() {
     system("cls");
@@ -24,6 +33,8 @@ int welcome_screen(){
     sleep(1);
     cout << "Gathering system information...\n";
     sleep(1);
+
+    // Get OS Version
     if(system("ver")){
         cout << system("ver\n");
     }else if(_WIN64 && _WIN32){
@@ -34,5 +45,61 @@ int welcome_screen(){
         cout << "Please consider using WINDOWS Operating System.\n";
         return 1;
     }
+
+    // Get Client LAN Interface Properties
+    client_ip      = getIPAddress();
+    client_subnet  = getSubnet();
+    cout << "Client IP:\t" << getIPAddress() << "\n";
+    cout << "Client Subnet:\t" << getSubnet() << "\n";
+    system(("ping "+client_ip).c_str());
+    cout << "Ready.\n";
+    sleep(1);
+    system("cls");
     return 0;
+}
+
+string getIPAddress(){
+    system("ipconfig > ip.db");
+    string container;
+    ifstream IPConfig;
+    int offset;
+    string search = "IPv4 Address";
+    IPConfig.open("ip.db");
+    if(IPConfig.is_open()){
+        while(!IPConfig.eof()){
+            getline(IPConfig, container);
+            if((offset = container.find(search)) != string::npos){
+                container.erase(0, container.find(":") + 2);
+                return container; 
+                IPConfig.close();
+                remove("ip.db");
+            }
+        }
+    }
+    IPConfig.close();
+    remove("ip.db");
+    return "Not Assigned";
+}
+
+string getSubnet(){
+    system("ipconfig > ip.db");
+    string container;
+    ifstream IPConfig;
+    int offset;
+    string search = "Subnet Mask";
+    IPConfig.open("ip.db");
+    if(IPConfig.is_open()){
+        while(!IPConfig.eof()){
+            getline(IPConfig, container);
+            if((offset = container.find(search)) != string::npos){
+                container.erase(0, container.find(":") + 2);
+                IPConfig.close();
+                remove("ip.db");
+                return container; 
+            }
+        }
+    }
+    IPConfig.close();
+    remove("ip.db");
+    return "Not Assigned";
 }
